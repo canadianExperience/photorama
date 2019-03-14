16d73e7d90401c6e26bd7e56345091e32ffa11aa
canadianExperience <lena2157@mail.ru>
2019-03-14 18:50:49 -0400

    Initial Commit

---
+++ Photorama/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Photorama
+//
+//  Created by Elena Melnikova on 2019-03-14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
