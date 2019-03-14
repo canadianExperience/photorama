caf2b9475c513e45b19eb7eccc057a3805db1885
canadianExperience <lena2157@mail.ru>
2019-03-14 19:15:37 -0400

    Navigation controller created

---
+++ Photorama/PhotosViewController.swift
@@ -0,0 +1,14 @@
+//
+//  PhotosViewController.swift
+//  Photorama
+//
+//  Created by Elena Melnikova on 2019-03-14.
+//
+
+import UIKit
+class PhotosViewController: UIViewController{
+   @IBOutlet var imageView: UIImageView!
+
+}
+
+
