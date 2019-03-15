0be2a8600e2d4b4035f44eade9e32b1c3e6d3a61
canadianExperience <lena2157@mail.ru>
2019-03-14 21:47:10 -0400

    Final commit

--- Photorama/PhotoStore.swift
+++ Photorama/PhotoStore.swift
@@ -6,6 +6,16 @@
 //
 
 import Foundation
+import UIKit
+
+enum ImageResult {
+    case success(UIImage)
+    case failure(Error)
+}
+
+enum PhotoError: Error{
+    case imageCreationError
+}
 
 enum PhotosResult{
     case success([Photo])
@@ -19,11 +29,12 @@ class PhotoStore{
         return URLSession(configuration: config)
     }()
     
-    func fetchInterestingPhotos(){
+    func fetchInterestingPhotos(completion: @escaping (PhotosResult) -> Void){
         let url = FlickrAPI.interestingPhotosURL
         let request = URLRequest(url: url)
         let task = session.dataTask(with: request){
             (data, response, error) -> Void in
+            
             if let jsonData = data{
                 do{
                     let jsonObject = try JSONSerialization.jsonObject(with: jsonData, options: [])
@@ -36,6 +47,11 @@ class PhotoStore{
             } else {
                 print("Unexpected error with the request")
             }
+            let result = self.processPhotosRequest(data: data, error: error)
+            OperationQueue.main.addOperation {
+                completion(result)
+            }
+            
         }
         task.resume()
     }
@@ -45,6 +61,36 @@ class PhotoStore{
             return .failure(error!)
         }
         return FlickrAPI.photos(fromJSON: jsonData)
+    }
+    
+    func fetchImage(for photo: Photo, completion: @escaping (ImageResult) -> Void){
+        let photoURL = photo.remoteURL
+        let request = URLRequest(url: photoURL)
+        
+        let task = session.dataTask(with: request){
+            (data, response, error) -> Void in
+            
+            let result = self.processImageRequest(data: data, error: error)
+            OperationQueue.main.addOperation {
+                 completion(result)
+            }
+           
+        }
+        task.resume()
+    }
+    
+    private func processImageRequest(data: Data?, error: Error?) -> ImageResult{
+        guard
+        let imageData = data,
+            let image = UIImage(data: imageData) else{
+                //Couldn't create an image
+                if data == nil {
+                    return .failure(error!)
+                } else {
+                    return .failure(PhotoError.imageCreationError)
+                }
+        }
+        return .success(image)
     }
     
     
